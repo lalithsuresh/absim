5f8754d475662d10b3d1b3560a286e4af601f9b1
Jing Li <jingli@mac-1N0-357.local>
2015-01-14 11:58:03 +0100

    add reissue

--- simulations/stage.py
+++ simulations/stage.py
@@ -8,12 +8,16 @@ from parallel import Parallel
 
 
 class Stage(object):
-    def __init__(self, env, id_, serverlist, replicaSelectionStrategy, composition=None):
+    def __init__(self, env, id_, serverlist, replicaSelectionStrategy, composition=None, timeout=None):
         self.env = env
         self.id_ = id_
         self.serverlist = serverlist
         self.replicaSelectionStrategy = replicaSelectionStrategy
         self.composition = composition
+        self.timeout = timeout
+        self.ctrl_reissue = env.event()
+        self.ctrl_succeed = env.event()
+        # self.ifReissue = ifReissue
         # self.action = env.process(self.excute())
 
     def getReplica(self):
@@ -30,10 +34,49 @@ class Stage(object):
             assert False, "REPLICA_SELECTION_STRATEGY isn't set or is invalid"
         return replicaSet
 
+    def reissue_ctrl(self):
+        # if self.timeout is not None:
+            global reissue
+            reissue = None
+            try:
+                yield self.env.timeout(self.timeout)
+                # self.ctrl_reissue.succeed()
+                # self.ctrl_reissue = env.event()
+                reissue = self.env.process(self.excute())
+                yield reissue
+                print("reissue finished first.")
+            except simpy.Interrupt as i:
+                print('at ', env.now, i.cause)
+                if reissue is not None:
+                    if not reissue.processed:
+                        reissue.interrupt('interrupt')
+
+
     def excute(self):
-        replicaToServe = self.getReplica()
-        yield self.env.process(replicaToServe.run(self.composition))
-        print('Stage "%d" run and finished itself after all its dependency Stages ' % self.id_)
+        try:
+            replicaToServe = self.getReplica()
+            wait_composition = self.env.process(replicaToServe.run(self.composition))
+            # reissue_ctrl = self.env.process(self.reissue_ctrl())
+            if self.timeout is not None:
+                reissue_ctrl = self.env.process(self.reissue_ctrl())
+                yield wait_composition | reissue_ctrl
+                if not wait_composition.processed:
+                    # reissue = self.env.process(self.excute())
+                    wait_composition.interrupt("interrupt")
+                    # wait_composition.fail(e)
+                    print("sent interrupt msg")
+                    # yield wait_composition | reissue
+                    # if not wait_composition.triggered:
+                    #     wait_composition.interrupt("interrupt")
+                if not reissue_ctrl.processed:
+                    reissue_ctrl.interrupt("reissue interrupt")
+            else:
+                yield wait_composition
+            print('Stage', self.id_, 'run and finished itself after all its dependency Stages at ', env.now)
+        except simpy.Interrupt as i:
+            print(self.id_, 'interrupted at', env.now, 'msg:', i.cause)
+            print('Stage', self.id_, 'terminated itself after all its dependency Stages at ', env.now)
+
 
 
 class Server():
@@ -60,32 +103,34 @@ class Server():
 
     def run(self, composition):
         servT = self.getServiceTime()  # Mu_i
-        if composition is not None:
-            yield self.env.process(composition.excute())
-        print('Server "%s" run at %d' % (self.id_, self.env.now))
-        yield self.env.timeout(servT)
-        print('Server "%s" finish at %d' % (self.id_, self.env.now))
+        try:
+            if composition is not None:
+                yield self.env.process(composition.excute())
+            print('Server "%s" run at %d' % (self.id_, env.now))
+            yield self.env.timeout(servT)
+            print('Server "%s" finish at %d' % (self.id_, env.now))
+        except simpy.Interrupt as i:
+            print(self.id_, 'interrupted at', env.now, 'msg:', i.cause)
+
+        # ctrl_succeed.succeed()
+        # # ctrl_succeed = env.event()
 
 env = simpy.Environment()
 server1 = Server(env, "server1", 5, "constant")
 server2 = Server(env, "server2", 10, "constant")
 server3 = Server(env, "server3", 1, "constant")
 server4 = Server(env, "server4", 6, "constant")
-s2 = Stage(env, 2, [server2], "random")
+s2 = Stage(env, 2, [server2, server3], "random")
 s3 = Stage(env, 3, [server3], "random")
-s4 = Stage(env, 4, [server4], "random")
-# s1 = Stage(env, 1, [server1], "random", Sequential(env, s2, s3))
-# env.process(s1.excute())
+s4 = Stage(env, 4, [server4, server3], "random")
+s1 = Stage(env, 1, [server1], "random", Sequential(env, s2, s3), 8)
+
+env.process(s1.excute())
+env.process(s1.excute())
 # s_parallel = Stage(env, 1, [server1], "random", Parallel(env, s2, s3))
 # env.process(s_parallel.excute())
-s_more = Stage(env, 1, [server1], "random", Sequential(env, s2, Parallel(env, s3, s4)))
-env.process(s_more.excute())
-env.run(until=50)
-
-
-
-
-
-
-
+# s_more = Stage(env, 1, [server1], "random", Sequential(env, s2, Parallel(env, s3, s4)), 4)
+# env.process(s_more.excute())
+# env.process(s_more.reissue_ctrl())
+env.run(until=20)
 
