6786646c1eadef90714ac8fd99dc2035f5bb9dee
Lalith Suresh <suresh.lalith@gmail.com>
2015-02-21 16:15:22 +0100

    Commit before running new factorial sweep using parallel

--- factorial.py
+++ factorial.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 import sys
 
-uniqId = sys.argv[1]
+# uniqId = sys.argv[1]
 
 numClients = [150, 300]
 numServers = [50]
@@ -11,11 +11,12 @@ numWorkload = [200]
 workloadModel = ["poisson"]
 serverConcurrency = [4]
 serviceTime = [4]
+timeVaryingDrift = [3]
 # utilization = [0.4, 0.45]
-utilization = [0.99]
+utilization = [0.80, 0.70, 0.45]
 serviceTimeModel = ["random.expovariate"]
 replicationFactor = [3]
-selectionStrategy = ["expDelay", "pending"]
+selectionStrategy = ["clairvoyant", "expDelay", "pending", "round_robin"]
 rateInterval = [20]
 cubicC = [0.000004]
 cubicSmax = [10]
@@ -23,16 +24,15 @@ cubicBeta = [0.2]
 hysterisisFactor = [2]
 shadowReadRatio = [0.1]
 accessPattern = ["uniform"]
-nwLatencyBase = [2.0]
+nwLatencyBase = [0.25]
 nwLatencyMu = [0]
 nwLatencySigma = [0]
-simulationDuration = [60000]
-seed = [int(uniqId)]
+simulationDuration = [100000]
+seed = [1,2,3,4,5]
 numRequests = [600000]
 # expScenario = ["heterogenousStaticServiceTimeScenario"]
 expScenario = ["timeVaryingServiceTimeServers"]
-demandSkew = [0.0]
-highDemandFraction = [0.0]
+demandSkewArgs = [[0.0, 0.0], [0.2, 0.8], [0.2, 0.5]] # first param is the demand skew, second param is the high demand fraction
 # slowServerFraction = [0.3, 0.5, 0.7]
 # slowServerSlowness = [0.8, 0.5, 0.3]
 # intervalParam = [0]
@@ -40,14 +40,13 @@ highDemandFraction = [0.0]
 slowServerFraction = [0]
 slowServerSlowness = [0]
 intervalParam = [10, 50, 100, 200, 300, 500]
-timeVaryingDrift = [6]
 
 
-logFolder = "higher-utilization-restart" + uniqId
+# logFolder = "higher-utilization-restart" + uniqId
 # logFolder = "paperSkewSweep" + uniqId
 
-if not os.path.exists(logFolder):
-        os.makedirs(logFolder)
+#if not os.path.exists(logFolder):
+#        os.makedirs(logFolder)
 
 LIST = [numClients,
         numServers,
@@ -73,8 +72,7 @@ LIST = [numClients,
         seed,
         numRequests,
         expScenario,
-        demandSkew,
-        highDemandFraction,
+        demandSkewArgs,
         slowServerFraction,
         slowServerSlowness,
         intervalParam,
@@ -97,18 +95,23 @@ for combination in PARAM_COMBINATIONS:
             nwLatencyMu, nwLatencySigma, \
             simulationDuration, seed, \
             numRequests, expScenario, \
-            demandSkew, highDemandFraction, \
+            demandSkewArgs, \
             slowServerFraction, \
             slowServerSlowness, intervalParam, \
             timeVaryingDrift, = combination
 
-        os.chdir(basePath + "/simulations")
+        #os.chdir(basePath + "/simulations")
         backpressure = ""
 
         if (selectionStrategy == "expDelay"
            or selectionStrategy == "round_robin"
            or selectionStrategy == "random"):
             backpressure = "--backpressure"
+
+        demandSkew, highDemandFraction = demandSkewArgs
+
+        logFolder = '_'.join(map(lambda x : str(x), combination))
+	logFolder = logFolder.replace('[', '').replace(']', '').replace(', ', '_')
 
         cmd = "python factorialExperiment.py \
                 --numClients %s\
@@ -176,29 +179,30 @@ for combination in PARAM_COMBINATIONS:
                      timeVaryingDrift,
                      logFolder,
                      backpressure)
-        proc = subprocess.Popen(cmd.split(),
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        out, err = proc.communicate()
+        print cmd
+        #proc = subprocess.Popen(cmd.split(),
+        #                        stdin=subprocess.PIPE,
+        #                        stdout=subprocess.PIPE,
+        #                        stderr=subprocess.PIPE)
+        #out, err = proc.communicate()
 
-        if (proc.returncode != 0):
-            print ' '.join(map(lambda x: str(x), combination)) + " ERROR"
-        else:
-            os.chdir(basePath + "/plotting")
-            cmd = "Rscript factorialResults.r %s %s"\
-                % (selectionStrategy, logFolder)
-            proc = subprocess.Popen(cmd.split(),
-                                    stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-            out, err = proc.communicate()
-            for line in out.split("\n"):
-                if (selectionStrategy in line):
-                    parts = line.split()
-                    for i in range(len(parts)):
-                        parts[i] = parts[i][1:-1]
-                    print ' '.join(map(lambda x: str(x), combination))\
-                        + " " + ' '.join(parts)
-                    sys.stdout.flush()
-        os.chdir(basePath)
+        #if (proc.returncode != 0):
+        #    print ' '.join(map(lambda x: str(x), combination)) + " ERROR"
+        #else:
+        #    os.chdir(basePath + "/plotting")
+        #    cmd = "Rscript factorialResults.r %s %s"\
+        #        % (selectionStrategy, logFolder)
+        #    proc = subprocess.Popen(cmd.split(),
+        #                            stdin=subprocess.PIPE,
+        #                            stdout=subprocess.PIPE,
+        #                            stderr=subprocess.PIPE)
+        #    out, err = proc.communicate()
+        #    for line in out.split("\n"):
+        #        if (selectionStrategy in line):
+        #            parts = line.split()
+        #            for i in range(len(parts)):
+        #                parts[i] = parts[i][1:-1]
+        #            print ' '.join(map(lambda x: str(x), combination))\
+        #                + " " + ' '.join(parts)
+        #            sys.stdout.flush()
+        #os.chdir(basePath)
