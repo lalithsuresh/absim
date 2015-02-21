86130f46609ce38ef816ae9b1bbb65492c20e0b5
Lalith Suresh <suresh.lalith@gmail.com>
2015-02-21 17:07:10 +0100

    Fix factorialResults.r

--- r-scripts/factorialResults.r
+++ r-scripts/factorialResults.r
@@ -1,14 +1,14 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-prefix <- args[1]
-dir <- args[2]
+dir <- args[1]
+prefix <- args[2]
 
 latency <- read.table(paste("../", dir, "/", prefix, "_Latency", sep=""))
 colnames(latency)[1] <- "ServerId"
 colnames(latency)[2] <- "Timestamp"
 colnames(latency)[3] <- "Latency"
 
-subset <- latency[latency$Timestamp > 2000,]
+# subset <- latency[latency$Timestamp > 2000,]
 
 options(width=10000)
-print(c(dir, prefix, mean(subset$Latency), quantile(subset$Latency,c(0.5,0.95, 0.99, 0.999) ) ) )
+print(c(prefix, mean(subset$Latency), quantile(subset$Latency,c(0.5,0.95, 0.99, 0.999) ) ) )
